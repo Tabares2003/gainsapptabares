afc1477e79167a567146b43c0d6bef51d3980d09
Tabares2003 <juanpablorojas200308@gmail.com>
2026-06-25 22:45:58 -0500

    act  datos arreglar 1

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import firebaseApp from '../firebase/credenciales';
-import { getAuth, signOut } from "firebase/auth";
+//import { getAuth } from "firebase/auth";
 
 import AdminView from '../components/adminView';
 import UserView from '../components/userView';
 
-const auth = getAuth(firebaseApp);
+//const auth = getAuth(firebaseApp);
 
 
 function Home({ user }) {
